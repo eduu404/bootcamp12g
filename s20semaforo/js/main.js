d0ec80da4b885e1193a3176fade32b929cde180b
eduu404 <eddie.pzg@gmail.com>
2021-07-13 22:16:08 -0500

    ultima act

---
+++ s20semaforo/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#botonRojo").click(function(){
+        $('body').css("background-color", "Red");
+    });
+    $("#botonAmarillo").click(function(){
+        $('body').css("background-color", "yellow");
+    });
+    $("#botonVerde").click(function(){
+        $('body').css("background-color", "green");
+    });
+});
+
