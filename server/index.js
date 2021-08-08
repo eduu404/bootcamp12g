ca5a9386e96e50f98696211346f34cfece434b26
eduu404 <eddie.pzg@gmail.com>
2021-08-07 23:05:40 -0500

    x

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+
+const server = http.createServer((request, response) =>{
+    console.log('path: ', request.url)
+    console.log('method: ', request.method)
+    response.write('Hola desde mi servidor Node')
+    response.end()
+})
+
+
+server.listen(8080, () => {
+    console.log('Server running in http://localhost:8080')
+})
+
