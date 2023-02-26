47edd95b3ae3b52785987b81feacee6044fecbcb
eduu404 <eddie.pzg@gmail.com>
2023-02-26 15:33:59 -0600

    3 ejercicios resueltos

--- s10javascript/js/main.js
+++ s10javascript/js/main.js
@@ -23,23 +23,38 @@ console.log( "La cadena mayor es: " + compararCadena(cadena1, cadena2) )
     salida: "A VeRy lArGe sTrInG"
     */
 
+    /*
     function zigzag(cadena){
         var longitudCadena = cadena.length
         var salida = "";
         let i
         for(i = 0; i <= longitudCadena; i++){
-            if(i%=0){
-                letra = cadena.charAt(i)
+            if(i%2==0){
+                let letra = cadena.charAt(i)
                 salida = salida + letra.toLowerCase()
             }else{
-                letra = cadena.charAt(i)
+                let letra = cadena.charAt(i)
                 salida = salida + letra.toUpperCase()
             }
-
         }
         return salida
     }
 
     cadena = prompt("Escribe una cadena:")
+    alert( zigzag(cadena) )*/
 
-    alert( zigzag(cadena) )
+    /*EJERCICIO 3: Crear un nuevo string y devolverlo al revés.
+    */
+
+    function invertirCadena(cadena){
+        var sizeCadena = cadena.length
+        var cadenaInversa = "";
+        for(sizeCadena; sizeCadena > 0; sizeCadena--){
+            let valorIndice = cadena.charAt(sizeCadena-1)
+            cadenaInversa = cadenaInversa + valorIndice
+        }
+        return cadenaInversa
+    }
+
+    var cadena = prompt("Escribe una cadena:")
+    alert( "La cadena invertida es: " + (invertirCadena(cadena)) )
