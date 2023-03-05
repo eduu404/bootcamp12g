717bca3fbcc6f7d61b0e4f299173f244b4f0666e
eduu404 <eddie.pzg@gmail.com>
2023-03-05 15:50:27 -0600

    ejercicio1 s11 resuelto

--- s11_conditionals/js/main.js
+++ s11_conditionals/js/main.js
@@ -4,25 +4,87 @@
 function evaluarInicial(cadena){
     var inicial = cadena.charAt(0)
     var tipoInicial = "";
+    var ascii = inicial.charCodeAt()
     console.log(inicial)
     if((inicial => 0) && (inicial <= 9)){
-        console.log("La cadena " + cadena + " comienza con el número: " + inicial)
-    } else if((inicial => "a") && (inicial <= "z")){
-        tipoInicial = "letra"
-        }else{
-            tipoInicial = "simbolo"
-        }
-    
-    if(tipoInicial = "letra"){
-        if(inicial = "a"){
-            console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
-            }else{
-                console.log("La cadena " + cadena + " comienza con la consonante: " + inicial)
-            }
-        if(tipoInicial = "simbolo"){
-            console.log("La cadena " + cadena + " comienza con el carácter especial: " + inicial)
-        }
+        tipoInicial = "numero"
+    }else if((ascii > 64) && (ascii < 91)){
+        tipoInicial = "letraMayuscula"
+    }else if((ascii > 96) && (ascii < 123)){
+        tipoInicial = "letraMinuscula"
+    }else{
+        tipoInicial = "simbolo"
     }
+    console.log(tipoInicial)
+
+    switch(tipoInicial){
+        case 'numero':
+            console.log("La cadena " + cadena + " comienza con el número: " + inicial)
+        break;
+
+        case 'letraMayuscula':
+            switch(inicial){
+                case 'A':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'E':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'I':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'O':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'U':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                default:
+                    console.log("La cadena " + cadena + " comienza con la consonante: " + inicial)
+                break;
+            }
+        break;
+
+        case 'letraMinuscula':
+            switch(inicial){
+                case 'a':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'e':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'i':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'o':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                case 'u':
+                    console.log("La cadena " + cadena + " comienza con la vocal: " + inicial)
+                break;
+
+                default:
+                    console.log("La cadena " + cadena + " comienza con la consonante: " + inicial)
+                break;
+            }
+        break;
+
+        case 'simbolo':
+            console.log("La cadena " + cadena + " comienza con el símbolo: " + inicial)
+        break;
+
+        default:
+        break;
+        }
 }
 
 cadena = prompt("Escribe una cadena:")
@@ -30,10 +92,9 @@ evaluarInicial(cadena)
 
 
 
+/* EJERCICIO 2: Inferir qué se debe hacer con la entrada para que dé las siguientes salidas.
 
-
-
-/*Input: anaconda
+    Input: anaconda
     output: true
 
     Input: belzeebub
@@ -44,4 +105,12 @@ evaluarInicial(cadena)
     
     input: cerveza
     output: false
-*/
+
+
+function evaluarCadena(cadena){
+    cadena.charAt(0) === cadena.charAt(cadena.length -1) ? firstEqualLast = true : firstEqualLast = false
+    return firstEqualLast
+}
+
+cadena = prompt("Escribe una palabra:")
+alert(evaluarCadena(cadena))*/
