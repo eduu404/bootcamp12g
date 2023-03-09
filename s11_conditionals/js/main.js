0bfdba692cf30c48e5da8f691a5cb8d5acd759d0
eduu404 <eddie.pzg@gmail.com>
2023-03-09 02:14:14 -0600

    ejercicio 5 resuelto

--- s11_conditionals/js/main.js
+++ s11_conditionals/js/main.js
@@ -1,6 +1,6 @@
 /* Crear una función que me diga si un string comienza con vocal, consonante, número o un carácter especial.
 */
-
+/*
 function evaluarInicial(cadena){
     var inicial = cadena.charAt(0)
     var tipoInicial = "";
@@ -88,7 +88,7 @@ function evaluarInicial(cadena){
 }
 
 cadena = prompt("Escribe una cadena:")
-evaluarInicial(cadena)
+evaluarInicial(cadena)*/
 
 
 /* ESTA ES UNA FORMA OPTIMIZADA PARA RESOLVER EL EJERCICIO DE ARRIBA:
@@ -138,3 +138,50 @@ function evaluarCadena(cadena){
 
 cadena = prompt("Escribe una palabra:")
 alert(evaluarCadena(cadena))*/
+
+
+
+
+/*EJERCICIO 5: Dada la longitud de una cadena por el usuario, generar sus caracteres de manera aleatoria sin que ninguno se repita.
+-creamos una variable para guardar la cadena resultante.
+-obtener el número de caracteres.
+-definir un rango numérico que determine el código ascii de los caracteres
+que estarán disponibles.
+-obtener de forma aleatoria algún número dentro de ese rango.
+-obtener el carácter correspondiente al número seleccionado.
+-almacenamos en una variable el carácter obtenido.
+
+-evaluamos si el carácter existe en la cadena de resultado.
+-si el caracter no existe, lo agregamos a la cadena de resultado.
+-si el caracter existe en la cadena, debemos generar un nuevo caracter.
+-repetir n veces.
+-devolver la cadena de resultado.
+*/
+
+
+function evaluarCadena(longitud){
+    var result = "";
+
+    do{
+      
+      caracter = generarAleatorio()
+
+      if(!result.includes(caracter)){
+      result += caracter
+      } else {
+        generarAleatorio()
+      }
+    }
+    while(result.length < longitud )
+  
+    return result
+}
+
+function generarAleatorio(){
+        var characterCode = Math.floor( Math.random() * (91 - 48) ) + 48
+    var character = String.fromCharCode(characterCode)
+  return character
+      }
+
+longitud = prompt("Escribe la longitud de la cadena:")
+console.log( evaluarCadena(longitud) )
