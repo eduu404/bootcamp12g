97cfe4dc3c747ac5a43f05b41dec0a2c6a04a406
eduu404 <eddie.pzg@gmail.com>
2023-03-16 01:44:11 -0600

    s12_arrays

---
+++ s12_arrays/js/main.js
@@ -0,0 +1,29 @@
+let someArray = []
+console.log(someArray)
+
+someArray.push("Misael","Jesus","Xoch","Mike","Jon")
+console.log(someArray)
+
+someArray.pop()
+console.log(someArray)
+
+someArray.shift()
+console.log(someArray)
+
+someArray.splice(1,1,"Freddy","Gerry")
+console.log(someArray)
+
+someArray.splice(1,0,"Bulmaro","Antonio")
+console.log(someArray)
+
+/*
+    crear una función que permita indicar la cantidad de koders a guardar,
+    que pida tantos nombres de koders como se indique, y al final imprima
+    el nombre del koder y su número asignado
+
+    Input: "Karen", "Xoch", "Wendy"
+    output:
+    Koder1: "Karen",
+    Koder 2: "Xoch",
+    Koder 3: "Wendy"
+*/
