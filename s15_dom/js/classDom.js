f2533acbd48a35ddc7325be3db03b27a978ef251
eduu404 <eddie.pzg@gmail.com>
2025-04-10 01:11:41 -0600

    9-abr-25

--- s15_dom/js/classDom.js
+++ s15_dom/js/classDom.js
@@ -2,9 +2,35 @@ let navbar = document.querySelector('nav');
 let navbar2 = document.getElementsByTagName('a');
 console.log(typeof(navbar2))
 console.log(Array.from(navbar2))
-console.log(typeof(navbar2))
 
+let elem1 = document.getElementById('e1');
+console.log(elem1)
+
+let elem3 = document.getElementsByClassName('tres');
+console.log(elem3)
+
+let elemQuery = document.querySelector('#e2');
+console.log(elemQuery)
+
+let elemQuery2 = document.querySelector('.tres');
+console.log(elemQuery2)
+
+let elemQuery3 = document.querySelectorAll('a');
+console.log(elemQuery3)
+
+let elemList = elemQuery3.forEach((elemProv)=>{
+    console.log(elemProv)
+})
 
 /*
 console.log(navbar)
 console.log(navbar2)*/
+
+let parrafo = document.createElement('p');
+let div = document.createElement('div');
+let body = document.querySelector('body');
+let padre = document.querySelector('#padre')
+
+body.appendChild(div);
+padre.appendChild(div);
+padre.insertBefore(parrafo, div);
