ebb9a0f4554d09065c2271adf7c2207146b2ef38
eduu404 <eddie.pzg@gmail.com>
2025-04-25 03:17:06 -0600

    update 25abr

--- s18.5eventListeners2/js/main.js
+++ s18.5eventListeners2/js/main.js
@@ -10,47 +10,67 @@ let pArray2 = [
     }
 ]
 
-let pArray = [];
+const pArray = [];
 let saveButton = document.getElementById('guardar');
-
-
 
 saveButton.addEventListener('click', () => {
     let pObj = {};
     let firstnameInput = document.getElementById('firstname-input');
     let lastnameInput = document.getElementById('lastname-input');
-    pObj.fname = firstnameInput.value
-    pObj.lstname = lastnameInput.value
-    console.log(pObj.fname)
-    console.log(pObj.lstname)
+    pObj.name = firstnameInput.value
+    pObj.lastname = lastnameInput.value
+    console.log(pObj.name)
+    console.log(pObj.lastname)
     pArray.push(pObj)
     console.log(pObj)
-    event.preventDefault();
+    console.log(pArray)
+    //event.preventDefault();
     printTable(pArray)
 })
+
+const deletePerson = event =>{
+    let personIndex = event.target.dataset.personIndex
+    pArray.splice(personIndex,1)
+    printTable(pArray)
+}
 
 const printTable = dataToPrint =>{
     document.getElementById("person-table").innerHTML = "";
     dataToPrint.forEach((item, index) =>{
-        let {name, lastname} = item
+    let {name, lastname} = item
     let personRow = document.createElement("tr")
     let indexTd = document.createElement("td")
+    indexTd.classList.add("tdNum")
     let nameTd = document.createElement("td")
-    nameTd.classList.add("table-dark")
+    nameTd.classList.add("table-dark", "table-striped", "tdNombres")
     let lastnameTd = document.createElement("td")
-    lastnameTd.classList.add("table-dark")
+    lastnameTd.classList.add("table-dark", "table-striped", "tdNombres")
+    let buttonTd = document.createElement("td")
+    buttonTd.classList.add("table-dark", "table-striped", "tdNombres")
 
     let nameText = document.createTextNode (name)
     let lastnameText = document.createTextNode (lastname)
     let indexText = document.createTextNode (index + 1)
 
+    let delButton = document.createElement("button")
+    delButton.classList.add("btn","del")
+    delButton.dataset.personIndex = index
+
+    delButton.addEventListener("click", deletePerson)
+
+    let buttonText = document.createTextNode("Borrar")
+    delButton.appendChild(buttonText)
+    buttonTd.appendChild(delButton)
+
     indexTd.appendChild(indexText)
     nameTd.appendChild(nameText)
     lastnameTd.appendChild(lastnameText)
+    //buttonTd.appendChild(buttonText)
 
     personRow.appendChild(indexTd)
     personRow.appendChild(nameTd)
     personRow.appendChild(lastnameTd)
+    personRow.appendChild(buttonTd)
 
     document.getElementById("person-table").appendChild(personRow)
     })
