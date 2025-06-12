fa0a27ddc15d7276aff4aafc4cfbe7252c5885fd
eduu404 <eddie.pzg@gmail.com>
2025-06-11 20:56:09 -0600

    update ajax

--- s19_ajax/js/main.js
+++ s19_ajax/js/main.js
@@ -4,21 +4,25 @@ const ajax_status = document.getElementById("ajax_status")
 const actualizarGato = () => { 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
+        console.log('status', this.status)
         
-        if(this.readyState ==3){
-            ajax_status.innerHTML="Cargando.."
-        }
         if(this.readyState == 4 && this.status == 200){
-            const url = JSON.parse(xhttp.responseText).file
-            imagen.setAttribute("src", url)
+            const RespuestaAjax = xhttp.responseText
+            console.log(RespuestaAjax)
+            const respuestaJS = JSON.parse(RespuestaAjax)
+            console.log(respuestaJS)
+
+            const url = respuestaJS.file
+            console.log(url)
+            imagen.setAttribute('src', url)
             ajax_status.innerHTML = ""
         }
-        if(this.readyState == 4 && this.status == 404){
-            imagen.setAttribute("src", url)
-        }
-    }
-    xhttp.open("GET", "https://aws.random.cat/meow", true);
+    };
+    xhttp.open("GET", "https://picsum.photos", true);
     xhttp.send();
 }
-
+//
 const btnActualizar = document.getElementById("btn_gato");
+btnActualizar.addEventListener('click', ()=>{
+    actualizarGato()
+})
